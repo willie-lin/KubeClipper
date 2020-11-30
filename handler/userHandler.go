96c552fab09a85c0471ce9f6f6dc4b336395dd46
yyhmmwan@gmail.com <yyhmmwan@gmail.com>
2020-11-30 18:14:55 +0800

    add config.go

--- handler/userHandler.go
+++ handler/userHandler.go
@@ -1,35 +1,32 @@
 package handler
 
 import (
-	"KubeClipper/model"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 // 注册新用户
 
-func regester(ctx *gin.Context)  {
+func regester(ctx *gin.Context) {
 
-	// 获取用户
-	user := &model.User{}
-
-	result :=&model.Result{
-		Code:	200,
-		Message:	"注册成功！",
-		Data:	nil,
-	}
-
-	if e := ctx.BindJSON(&user); e != nil {
-		result.Message = "数据绑定失败！"
-		result.Code = http.StatusUnauthorized
-		ctx.JSON(http.StatusUnauthorized, gin.H{
-			"result": result,
-		})
-	}
-	u := user.QueryUsername()
-	if u.Passwoed == user.Passwoed {
-
-	}
-
+	//// 获取用户
+	//user := &model.User{}
+	//
+	//result :=&model.Result{
+	//	Code:	200,
+	//	Message:	"注册成功！",
+	//	Data:	nil,
+	//}
+	//
+	//if e := ctx.BindJSON(&user); e != nil {
+	//	result.Message = "数据绑定失败！"
+	//	result.Code = http.StatusUnauthorized
+	//	ctx.JSON(http.StatusUnauthorized, gin.H{
+	//		"result": result,
+	//	})
+	//}
+	//u := user.QueryUsername()
+	//if u.Passwoed == user.Passwoed {
+	//
+	//}
 
 }
--- pkg/csconfig/config.go
+++ pkg/csconfig/config.go
@@ -1,1 +1,66 @@
 package csconfig
+
+//
+type GlobalConfig struct {
+	Self     *string      `yaml:"-"`
+	DbConfig *DatabaseCfg `yaml:"db_config,omitempty"`
+}
+
+//func (c *GlobalConfig)  Dump() error {
+//
+//	out, err := yaml.Marshal(c)
+//	if err != nil {
+//		return errors.Wrap(err, "failed marshaling config")
+//		}
+//	fmt.Printf("%s", string(out))
+//	return nil
+//}
+//
+//func (c *GlobalConfig) LoadConfigurationFile(path string) error  {
+//
+//	fcontent, err := ioutil.ReadFile(path)
+//	if err != nil {
+//		return errors.Wrap(err, "failed to read config file")
+//	}
+//	err = yaml.UnmarshalStrict(fcontent, c)
+//	if err != nil {
+//		return errors.Wrap(err, "failed unmarshaling config")
+//	}
+//	path, err = filepath.Abs(path)
+//	if err != nil {
+//		return errors.Wrap(err, "failed to load absolute path")
+//	}
+//	c.Self = &path
+//	if err := c.LoadConfiguration(); err != nil {
+//		return errors.Wrap(err, "failed to load sub configurations")
+//	}
+//	return nil
+//}
+//
+//func (c *GlobalConfig) LoadConfiguration() error {
+//
+//
+//}
+
+func NewConfig() *GlobalConfig {
+	cfg := GlobalConfig{}
+	return &cfg
+}
+
+func NewDefaultConfig() *GlobalConfig {
+	//logLevel  := log.InfoLevel
+
+	dbConfig := DatabaseCfg{
+		User:     "root",
+		Password: "root1234",
+		Host:     "127.0.0.1",
+		Port:     3306,
+		DbName:   "KubeClipper",
+	}
+	globalCfg := GlobalConfig{
+		DbConfig: &dbConfig,
+	}
+
+	return &globalCfg
+
+}
